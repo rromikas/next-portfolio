983f2ceb479ece3e2c12260fb0ca52256f8bd7da
rromikas <rromikas@gmail.com>
2023-01-04 16:43:00 +0200

    testing ifttt

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 interface PrivacyPolicyPageProps {}
 
 const PrivacyPolicyPage: FunctionComponent<PrivacyPolicyPageProps> = () => {
-  return <iframe src="/privacy-policy.html" className="fixed left-0 top-0 w-full h-full"></iframe>;
+  return <iframe title="privacy-policy" src="/pages/privacy-policy.html" className="fixed left-0 top-0 w-full h-full"></iframe>;
 };
 
 export default PrivacyPolicyPage;
